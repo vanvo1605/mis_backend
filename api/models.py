8399d6611d2370085356f72c5db11887e7391d9e
20231599 <vavo001@ess.ais.ac.nz>
2024-07-15 03:08:01 +1200

    add function for register

--- api/models.py
+++ api/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 class Semester(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,10 @@ from  django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from .views import sumNumbersView
-from .viewsets import StudentViewSet, SemesterViewSet, ProgramViewSet, EnrollmentViewSet, LecturerViewSet
+from .viewsets import StudentViewSet, SemesterViewSet, ProgramViewSet, EnrollmentViewSet, LecturerViewSet, UserViewSet
 
 router = DefaultRouter()
+router.register('user', UserViewSet)
 router.register('student', StudentViewSet)
 router.register('semester', SemesterViewSet)
 router.register('program', ProgramViewSet)
